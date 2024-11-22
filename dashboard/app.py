3ded836492880081611af76f5b4db8f0b9cec542
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-11-22 15:29:54 -0600

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -46,3 +46,25 @@ with ui.sidebar(open="open"):
         "A demonstration of real-time temperature readings in Antarctica.",
         class_="text-center",
     )
+
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+
+icon_svg("sun")
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
