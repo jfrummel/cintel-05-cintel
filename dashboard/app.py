0e508a32ceaad7207ee1933b1df2bca16a9d69aa
Jeremy Rummel <42078577+jfrummel@users.noreply.github.com>
2024-11-22 15:26:36 -0600

    Update app.py
    Initial commit with basic app with live data

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,48 @@
+from shiny import reactive, render
+from shiny.express import ui
 
+# Imports from Python Standard Library to simulate live data
+import random
+from datetime import datetime
+from faicons import icon_svg
+
+
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+UPDATE_INTERVAL_SECS: int = 1
+# --------------------------------------------
+
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+
+@reactive.calc()
+def reactive_calc_combined():
+
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
+
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+
+    # Return everything we need
+    return latest_dictionary_entry
+
+
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
